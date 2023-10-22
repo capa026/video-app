1e5d1b504e7a289e676170a37d6db00bbb3cf1c5
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-10-22 10:15:46 -0400

    Feed.jsx modified

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { SideBar } from '../components';
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack
+      sx={{
+        flexDirection: { sx: 'column', md: 'row' },
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ color: '#fff', mt: 1.5 }}
+        >
+          Copyright 2023 Fimbulwinter Games
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 export default Feed;
