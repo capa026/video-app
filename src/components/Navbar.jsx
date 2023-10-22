4c38283cd808898297d7e83fe0c5a6ba82de0705
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-10-22 10:16:07 -0400

    Files Modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
 
+import { logo } from '../utils/constants';
+import { SearchBar } from '../components';
+
 const Navbar = () => (
   <Stack
     direction="row"
@@ -10,10 +13,13 @@ const Navbar = () => (
       position: 'sticky',
       background: '#000',
       top: 0,
-      justifyContent: 'space=between',
+      justifyContent: 'space-between',
     }}
   >
-    hola
+    <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={logo} alt="logo" height={45} />
+    </Link>
+    <SearchBar />
   </Stack>
 );
 export default Navbar;
