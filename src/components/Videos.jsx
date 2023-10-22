7145de39877e686cb988205d6bd30f8540e0ebf3
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-10-22 15:52:46 -0400

    Files created

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,4 +1,13 @@
-const Videos = ({ videos }) => {
-  return <div>Videos</div>;
-};
+import { Stack, Box } from '@mui/material';
+
+const Videos = ({ videos }) => (
+  <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    {videos.map((item, index) => {
+      <Box key={index}>
+        {video.id.videoId && <VideoCard video={item} />}
+        {video.id.ChannelId && <ChannelCard channelDetail={item} />}
+      </Box>;
+    })}
+  </Stack>
+);
 export default Videos;
