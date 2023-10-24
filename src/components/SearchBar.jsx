6f04b681cffca647a40b6aae262e8bb637b38bc1
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-10-23 22:16:24 -0400

    ADDED: useState functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,9 @@ import { Paper, IconButton } from '@mui/material';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Search } from '@mui/icons-material';
+
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <Paper
       component="form"
@@ -19,8 +21,10 @@ const SearchBar = () => {
         type="text"
         className="search-bar"
         placeholder="Search"
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
       />
       <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
         <Search />
