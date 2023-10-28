6d4b43286331cc2928cc821a49df4e555ffd9789
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-10-28 07:40:49 -0400

    MODIFIED: Functionality added to the ChannelDetail component, now is able to show the videos related to the channel

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,4 +1,44 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  console.log(channelDetail);
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              'radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(55,18,73,1) 71%)',
+            zIndex: 10,
+            height: '300px',
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+
+        <Box display="flex" p="2">
+          <Box sx={{ mr: { sm: '100px' } }} />
+          <Videos videos={videos} />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 export default ChannelDetail;
