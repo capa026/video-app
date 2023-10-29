fdd3a719f7b8f6314495f10a5a49011c43e94456
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-10-28 20:42:43 -0400

    REMOVED: console.log()

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -11,7 +11,6 @@ const ChannelDetail = () => {
 
   const { id } = useParams();
 
-  console.log(channelDetail);
   useEffect(() => {
     fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
       setChannelDetail(data?.items[0])
