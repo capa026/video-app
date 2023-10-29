5508069cac93bc3ac18cb52ff4a1a65c167950de
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-10-29 18:46:30 -0400

    ADDED: Functionality

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,4 +1,55 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import { Video } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+
 const VideoDetail = () => {
-  return <div>VideoDetail</div>;
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => {
+      console.log(data);
+      setVideoDetail(data.items[0]);
+    });
+  }, [id]);
+
+  const {
+    snippet: { title, channelID, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: '#fff' }}
+              py={1}
+              px={2}
+            >
+              <Link to={`/channel/${channelID}`}>
+                <Typography>{channelTitle}</Typography>
+              </Link>
+            </Stack>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 export default VideoDetail;
