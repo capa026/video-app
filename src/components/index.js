954a94dd55e0548fe8db7183e6822e10326567c1
capa026 <118733637+capa026@users.noreply.github.com>
2023-10-29 21:11:10 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -8,4 +8,3 @@ export { default as SideBar } from './SideBar';
 export { default as Videos } from './Videos';
 export { default as VideoCard } from './VideoCard';
 export { default as ChannelCard } from './ChannelCard';
-export { default as Video } from './Video';
