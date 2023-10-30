ac7243208ac99907e5b0248cdeba1c8954d43005
Carlos Pacheco <carlospacheco.fx@gmail.com>
2023-10-29 20:55:29 -0400

    MODIFIED

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,24 +2,33 @@ import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
-import { CheckCircle } from '@mui/icons-material';
+import { CheckCircle, Visibility, ThumbUp } from '@mui/icons-material';
 
-import { Video } from './';
+import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => {
-      console.log(data);
       setVideoDetail(data.items[0]);
     });
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => {
+        setVideos(data.items);
+      }
+    );
   }, [id]);
 
+  if (!videoDetail?.snippet) return;
+
   const {
-    snippet: { title, channelID, channelTitle },
+    snippet: { title, channelId, channelTitle },
     statistics: { viewCount, likeCount },
   } = videoDetail;
   return (
@@ -31,6 +40,7 @@ const VideoDetail = () => {
               url={`https://www.youtube.com/watch?v=${id}`}
               className="react-player"
               controls
+              playing
             />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
@@ -42,11 +52,46 @@ const VideoDetail = () => {
               py={1}
               px={2}
             >
-              <Link to={`/channel/${channelID}`}>
-                <Typography>{channelTitle}</Typography>
+              <Link to={`/channel/${channelId}`}>
+                <Typography variant="subtitle" color="#fff">
+                  {channelTitle}
+                  <CheckCircle
+                    sx={{ fontSize: '12px', color: 'grey', ml: '5px' }}
+                  />
+                </Typography>
               </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography
+                  variant="body1"
+                  sx={{ opacity: 0.7 }}
+                  display="flex"
+                >
+                  <Visibility
+                    sx={{ fontSize: '20px', color: 'grey', mr: '5px' }}
+                  />
+                  {parseInt(viewCount).toLocaleString()} Views
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{ opacity: 0.7 }}
+                  display="flex"
+                >
+                  <ThumbUp
+                    sx={{ fontSize: '20px', color: 'grey', mr: '5px' }}
+                  />
+                  {parseInt(likeCount).toLocaleString()} Likes
+                </Typography>
+              </Stack>
             </Stack>
           </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
